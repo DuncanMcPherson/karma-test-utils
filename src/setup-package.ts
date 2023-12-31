f5d8f8cd016a7968e69ac64deb910f56c78aaa63
Duncan McPherson <duncan.mcpherson.jobs@gmail.com>
2023-12-30 20:33:16 -0700

    added package setup script

---
+++ src/setup-package.ts
@@ -0,0 +1,15 @@
+import fs from "fs";
+import path from "path";
+
+function main() {
+	const sourcePath = path.join(__dirname, "..", "package.json");
+	const source = fs.readFileSync(sourcePath).toString('utf-8');
+	const sourceObj = JSON.parse(source);
+
+	sourceObj.scripts = {};
+	sourceObj.devDependencies = {};
+	if (sourceObj.main.startsWith("dist/")) {
+		sourceObj.main = sourceObj.main.slice(5);
+	}
+	fs.writeFileSync(__dirname + "/package.json", Buffer.from(JSON.stringify(sourceObj, null, 2), "utf-8"));
+}
